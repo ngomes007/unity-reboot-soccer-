d3ad78b3255647f7dcada7cbd4f254450c150cfd
ngomes007 <32779546+ngomes007@users.noreply.github.com>
2020-04-09 12:16:24 -0700

    start

---
+++ Assets/ModoBridge/Scripts/FoundryMaterialStore.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+namespace Foundry
+{
+    public class FoundryMaterialStore : MonoBehaviour
+    {
+        public Material material;
+    }
+}
---
+++ Assets/ModoBridge/Scripts/FoundryUniqueToken.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEditor;
+
+namespace Foundry
+{
+    [ExecuteInEditMode]
+    public class FoundryUniqueToken : MonoBehaviour
+    {
+        public string uniqueToken;
+
+        public void Awake()
+        {
+            Scene scene = SceneManager.GetActiveScene();
+            if (scene.isLoaded)
+            {
+#if UNITY_2018_3_OR_NEWER
+                // Seems that the prefab paradigm has changed from this point on. No need to disconnect the prefab instance.
+#else
+                // TODO: Clicking on apply still overwrites the prefab even though we disconnect the prefab instance
+                // try to find a solution that doesn't break the prefab values.
+                PrefabUtility.DisconnectPrefabInstance(this);
+#endif
+                uniqueToken = "";
+            }
+        }
+    }
+
+}
